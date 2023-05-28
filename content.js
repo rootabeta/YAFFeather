e4b413531bf407ed09ed38b4caca17ad5566ed86
rootabeta <rootabeta@protonmail.com>
2023-05-28 17:29:11 -0400

    Migrated to new branch

---
+++ content.js
@@ -0,0 +1,129 @@
+document.addEventListener('keyup', function (event) { // keyup may seem less intuitive but it's already the standard in breeze-like scripts and it prevents holding down a key spamming the server
+	if (event.shiftKey || event.ctrlKey || event.altKey || document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') { // locks you out of the script while you're holding down a modifier key or typing in an input
+		return;
+	} else {
+		switch (event.code) { // event.code is the key that was pressed
+			case 'KeyA': // reload page
+				window.location.reload();
+				break;
+			case 'KeyT': // CUSTOM: Set template-overall to none
+				if (window.location.href.includes("template-overall=none")) {
+					window.location.reload();
+				} else { 
+					window.location.href = window.location.href + "/template-overall=none";
+				}
+				break;
+			case 'KeyQ': // go back
+				window.history.back();
+				break;
+			case 'KeyU': // check if you updated
+				window.location.assign("https://www.nationstates.net/page=reports/view=self/filter=change/template-overall=none");
+				break;
+			case 'KeyS': // endorse nation - NOT toggle endorsement! Together with my tampermonkey DidIEndo.js script, this should make endo confusion a thing of the past.
+				if (window.location.href.includes("nation=")) {
+					if (document.getElementsByClassName("endorse button icon wa danger").length == 0) { //Is the button to endorse, or does it have the unendo warning? If the latter, skip. Otherwise, endo
+						document.getElementsByClassName('endorse button icon wa')[0].click();
+					}
+				}
+				break;
+			case 'KeyL': // It's rare we need to explicitly unendo, I'm okay with putting it on the other side of the keyboard
+				if (window.location.href.includes("nation=")) {
+					document.getElementsByClassName('endorse button icon wa danger')[0].click(); //NOT endorse, ONLY unendorse has the danger modifier
+				}
+				break;
+			case 'KeyO': // ban nation
+				if (window.location.href.includes("nation=")) {
+					document.getElementsByName('ban')[0].click();
+				}
+				break;
+			case 'KeyK': // eject nation
+				if (window.location.href.includes("nation=")) {
+					document.getElementsByName('eject')[0].click();
+				}
+				break;
+			case 'KeyR': // confirm wa join
+				if (window.location.href.includes("page=join_WA")) {
+					var NationURL = document.getElementsByTagName("form")[1].getElementsByClassName("nlink")[0].href;
+					navigator.clipboard.writeText(NationURL);
+					document.getElementsByClassName('button primary icon approve big')[0].click();
+				}
+				break;
+			case 'KeyF': // move to region whose page you're currently on
+				if (window.location.href.includes("region=")) {
+					document.getElementsByName('move_region')[0].click();
+				}
+				break;
+			case 'KeyB': // move to suspicious
+				if (window.location.href == "https://www.nationstates.net/region=suspicious") {
+					document.getElementsByName('move_region')[0].click();
+				} else {
+					window.location.assign("https://www.nationstates.net/region=suspicious");
+				}
+				break;
+			case 'KeyE': // resign from WA, courtesy of NotAName
+				if (window.location.href.includes("https://www.nationstates.net/page=un")) {
+					if (document.getElementsByTagName("form")[1].getElementsByTagName("button")[0].textContent.includes("Apply to Join")) { 
+						document.getElementsByTagName("form")[1].getElementsByTagName("button")[0].click() // Apply to join
+					} else {  // Nota's absolutely villainous, devious trick
+						var chk = document.getElementsByName('chk')[0].value;
+						window.location.assign(`https://www.nationstates.net/page=UN_status?action=leave_un&submit=1&chk=${chk}`);
+					}
+				} else {
+					window.location.assign("https://www.nationstates.net/page=un");
+				}
+				break;
+			case 'KeyZ': // go to current region page
+				if (window.location.href == "https://www.nationstates.net/page=change_region") { // if on post-relocation page
+					//document.getElementsByClassName('info')[0].getElementsByClassName('rlink')[0].click()
+					document.getElementsByClassName('info')[0].getElementsByClassName('rlink')[0].click(); // click the region link on the relocation page
+				} else { // otherwise just click the region link through the sidebar
+					document.getElementById('panelregionbar').querySelector('a').click();
+				}
+				break;
+			case 'KeyX': // Copy the current nation to the clipboard
+				// If we are looking at the WA application page, grab the link from the application we're looking at instead of our current nation
+				// I've had issues where I hit X too quickly and it gives me the puppet I just switched off of - this should fix that.
+				if (window.location.href.includes("https://www.nationstates.net/page=join_WA?nation=") && document.getElementsByTagName("form").length > 1) { //Safety net, since I can't test WA right now - make sure we have a second form to query.
+					var NationURL = document.getElementsByTagName("form")[1].getElementsByClassName("nlink")[0].href; // First form is login banner - second form is application. Isolate the nation link and copy to clipboard. 
+				} else { 
+					var NationLink = document.getElementsByClassName("bellink quietlink");
+					var NationURL = NationLink[0].href;
+				}
+				navigator.clipboard.writeText(NationURL);
+				break;
+			case 'KeyD': // appoint yourself as and/or deappoint ROs
+				var current_nation = document.getElementById("loggedin").getAttribute("data-nname");
+				// If on the regional control page, open own regional officer page
+				if (window.location.href == "https://www.nationstates.net/page=region_control") {
+					window.location.assign("https://www.nationstates.net/page=regional_officer/nation=" + current_nation);
+				}
+				// If on on own regional officer page, assign officer role
+				else if (window.location.href == "https://www.nationstates.net/page=regional_officer/nation=" + current_nation) {
+					document.getElementsByName("office_name")[0].value = "Supreme Overlord";
+					document.getElementsByName("authority_A")[0].checked = true;
+					document.getElementsByName("authority_C")[0].checked = true;
+					document.getElementsByName("authority_E")[0].checked = true;
+					document.getElementsByName("authority_P")[0].checked = true;
+					document.getElementsByName('editofficer')[0].click();
+				}
+				// If on someone else's regional officer page, dismiss them (or strip permissions if successor)
+				else if (window.location.href.includes("regional_officer")) {
+					if(document.getElementsByName("authority_S")[0].checked) { // If has succession authority, remove all other permissions, since successors cannot be removed by exec WA. Thanks Nota!
+						document.getElementsByName("authority_A")[0].checked = false;
+						document.getElementsByName("authority_B")[0].checked = false;
+						document.getElementsByName("authority_C")[0].checked = false;
+						document.getElementsByName("authority_E")[0].checked = false;
+						document.getElementsByName("authority_P")[0].checked = false;
+						document.getElementsByName('editofficer')[0].click();						
+					} else { 
+						document.getElementsByName('abolishofficer')[0].click();
+					}
+				}
+				// If on none of these pages, open regional control page
+				else {
+					window.location.assign("https://www.nationstates.net/page=region_control");
+				}
+				break;
+		} // end switch
+	} // end else
+}); // end event listener
---
+++ readme.txt
@@ -0,0 +1,78 @@
+===============================
+YAFFeather v3.5
+
+Derivative extension of SweezeBreeze, with removed and added stuff, tailored towards raiders/tags
+
+================================
+KEY LIST:
+
+A - Refresh page
+T - Set template-overall=none for faster pageloads
+Q - Go back
+U - Check if your nation updated
+S - Endorse nation
+L - Unendorse nation
+O - Ban nation
+K - Eject region
+R - Confirm WA join
+F - Move to region in current tab
+B - Move to suspicious
+E - Apply/resign to/from WA
+Z - Go to current region page
+X - Copy current nation URL to clipboard
+D - Appoint yourself as RO/Dismiss other ROs
+===============================
+3.5.2
+Re-enabled applying to WA
+
+3.5.1
+Bugfixes on pressing Z on region move, and pressing E again after confirming a WA application
+
+3.5
+Seperated endorsements from unendorsements
+Made X copy the nation of the currently viewed WA application, if not yet fully applied 
+Made R copy the nation of the WA application to clipboard immediately before applying
+Made RO dismissal remove all authorities from successors, who cannot be fully removed by an executive WA (Thanks to NotAName for the code on this one)
+Brought keystrokes to resign from 3 to 2 (e e enter -> e e) (Thanks to NotAName for this code too)
+
+3.4
+Jumped the versioning shark during migration from stock Feather branch to YAFFeather
+Added template-overall functionality
+Changed nation URL copying from V to X
+Changed default RO title from Raider Unity to Supreme Overlord
+
+=======
+
+3.1
+prepared for publishing on chrome web store
+updated jump point to suspicious
+changed RO name
+---
+
+
+3.0
+Removed mousetrap, rewritten to use pure javascript instead
+---
+2.2
+Rewrote the script in Mousetrap due to the deprecation of the keyCode method as well as for code cleanliness sake, switched from jQuery in favor of Cash to save ~60KB
+---
+2.1
+Updated to current Feather - Alterations Made By Miravana
+Removed Dossier/Reports Functionality
+Changed Keybinds and Regional Information
+---
+2.0 
+Updated to current SweezeBreeze - Script By Sweeze
+---
+1.2
+Re-arranged keys so less hand moving necessary.
+Added icon.
+---
+1.1
+added regional officer functionality
+---
+1.0
+added confirm WA join feature
+fixed join/resign feature
+removed dossier+defender stuff
+changed spear danes to plum island+le club des cinq
