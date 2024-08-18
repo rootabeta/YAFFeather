011f2e3c35230e175abc75c4f118030ec934bf8a
rootabeta <rootabeta@protonmail.com>
2024-08-17 20:38:54 -0400

    Added buildfile from railgun

---
+++ build.sh
@@ -0,0 +1,4 @@
+VERSION=$(cat manifest.json | jaq .version | cut -d '"' -f 2 | tr '.' '_')
+echo "Building YAFFeather ${VERSION}"
+zip -r -FS release/yaffeather-${VERSION} * --exclude .git* release/*
+echo "Build completed"
