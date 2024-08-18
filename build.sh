6aa655e67dddee1ccf46135a81adfc35dfd256ea
rootabeta <rootabeta@protonmail.com>
2024-08-17 20:40:14 -0400

    Prevented adding build.sh to outfile

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 VERSION=$(cat manifest.json | jaq .version | cut -d '"' -f 2 | tr '.' '_')
 echo "Building YAFFeather ${VERSION}"
-zip -r -FS release/yaffeather-${VERSION} * --exclude .git* release/*
+zip -r -FS release/yaffeather-${VERSION} * --exclude .git* release/* build.sh
 echo "Build completed"
